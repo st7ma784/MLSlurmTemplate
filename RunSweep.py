ab88e3744112af00b3da82451bc8919278bf0ce9
Stephen <37083603+st7ma784@users.noreply.github.com>
2022-08-15 11:03:02 +0100

    Create RunSweep.py

---
+++ RunSweep.py
@@ -0,0 +1,13 @@
+  
+import wandb
+from train import train
+import argparse
+import wandb
+from functools import partial
+wandb.login()
+parser = argparse.ArgumentParser()
+parser.add_argument("--sweep_id", default="2kt9gdh0",nargs="?", type=str)
+parser.add_argument("--data_dir", default="/Data",nargs="?", type=str)
+p = parser.parse_args()
+train=partial(train,dir=p.data_dir)
+wandb.agent(sweep_id=p.sweep_id, project=<WANDBPROJECTNAME>, entity=<WANDBUSERNAME>,function=train)
