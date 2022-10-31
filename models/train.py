81cdb141061f97bfd4d1c655d007a283a8cfa707
Stephen <37083603+st7ma784@users.noreply.github.com>
2022-10-31 14:51:14 +0000

    Update train.py

--- models/train.py
+++ models/train.py
@@ -8,6 +8,9 @@ from typing import Optional
 from warnings import warn
 
 class myLightningModule(LightningModule):
+    '''
+    This training code follows the standard structure of Pytorch - lighthning. It's worth looking at their docs for a more in depth dive as to why it is this was
+    '''
     
     def __init__(self,
                 learning_rate,
@@ -22,7 +25,7 @@ class myLightningModule(LightningModule):
         super().__init__()
         self.save_hyperparameters()
         self.loss=torch.nn.CrossEntropyLoss()
-
+        #Define your own model here, 
         self.model=torch.nn.Sequential(*[
             torch.nn.Linear(50000,512),
             torch.nn.Linear(512,256),
@@ -31,13 +34,17 @@ class myLightningModule(LightningModule):
 
         ])
     def forward(self,input):
+        #This inference steps of a foward pass of the model 
         return self.model(input)
 
     def training_step(self, batch, batch_idx,optimizer_idx=0):
-        input,desired=batch[0],batch[1]
-        input=torch.nn.functional.one_hot(input,num_classes=self.input_vocab_size).to(torch.float)
+        #The batch is collated for you, so just seperate it here and calculate loss. 
+        #By default, PTL handles optimization and scheduling and logging steps. so All you have to focus on is functionality. Here's an example...
+        input,target=batch[0],batch[1]
         out=self.forward(input)
-        loss=self.loss(out,desired)
+        loss=self.loss(out,target)
+        
+        #Logging is done through this module as follows.
         self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
       
@@ -45,13 +52,14 @@ class myLightningModule(LightningModule):
     def validation_step(self, batch, batch_idx):
       
         input,desired=batch[0],batch[1]
-        input=torch.nn.functional.one_hot(input,num_classes=self.input_vocab_size).to(torch.float)
         out=self.forward(input)
-        print(out[0])
-        print(desired[0])
-
-    def configure_optimizers(self):
+        #You could log here the val_loss, or just print something. 
         
+    def configure_optimizers(self):
+        #Automatically called by PL. So don't worry about calling it yourself. 
+        #you'll notice that everything from the init function is stored under the self.hparams object 
         optimizerA = torch.optim.Adam(
             self.parameters(), lr=self.hparams.learning_rate, eps=1e-8)
+        
+        #Define scheduler here too if needed. 
         return [optimizerA]
