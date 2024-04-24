9d761e91b9b656e9a9dc8c514446f91188740d4a
Stephen <37083603+st7ma784@users.noreply.github.com>
2024-04-24 14:14:26 +0100

    Update Launch.py

--- Launch.py
+++ Launch.py
@@ -117,7 +117,7 @@ def SlurmRun(trialconfig):
     else: 
         
         sub_commands.extend(['#SBATCH -p gpu-medium',
-                             'export CONDADIR=/home/$USER/miniconda3',
+                             'export CONDADIR=/storage/hpc/46/manders3/conda4/open-ce',
                              'export NCCL_SOCKET_IFNAME=enp0s31f6',])
     sub_commands.extend([ '#SBATCH --{}={}\n'.format(cmd, value) for  (cmd, value) in slurm_commands.items()])
     sub_commands.extend([
